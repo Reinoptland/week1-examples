d5df5499719bce61d35a6e0afe5460b2facd96c2
reinoptland <reinoptland@gmail.com>
2019-04-26 12:01:21 +0200

    Loops and Scope

---
+++ loops.js
@@ -0,0 +1,40 @@
+// Loops
+// Why:
+// Avoid repetition
+// Keep going based on the state of the program
+
+
+// loops keyword: for, while, do while, for ... in, for ... of
+
+// const students = ['Tom', 'Kees', 'Drashti']
+
+// for ( initialize index; condition to keep going; final action; )
+// for (let index = 0; index < students.length; index++) {
+//     console.log("INDEX", index)
+//     const element = students[index];    
+//     console.log("ELEMENT", element)
+// }
+
+// let count = -1
+
+// // Runs only if the condition is true
+// // And continues as long as the condition is true
+// while (count === 0) {
+//     count++
+//     console.log("COUNT", count)
+// }
+
+
+// // Do while runs at least once
+// do {
+//     count++
+//     console.log("COUNT", count)
+// } while (count === 0);
+
+
+// My favorite:
+// for (const student of students) {
+//     console.log(student)
+// }
+
+
---
+++ scope.js
@@ -0,0 +1,19 @@
+let name = "Rein"
+console.log(name)
+
+
+{
+    
+    name = "Mimi"
+    const pet = "mr Sniffles"
+    console.log(name)
+    
+}
+
+console.log(name)
+
+const obj = {}
+
+// ES2015 a.k.a. ES6
+// console.log(pet)
+
